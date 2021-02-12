76c8a625f9fcb7ec04a048264d7373c5fb9e37df
Domna Kanari <domnakanari@gmail.com>
2021-02-12 21:40:07 +0200

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-test';
+  title = 'geosocial-tourism-2020-2021';
 }
